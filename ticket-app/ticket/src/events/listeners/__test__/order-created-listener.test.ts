eb71b8d48c6cb77b4f5d599938e10a39efa8bd50
certifiedTboy <etosin70@gmail.com>
2025-03-27 16:25:30 +0100

    update module

---
+++ ticket-app/ticket/src/events/listeners/__test__/order-created-listener.test.ts
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+import { Message } from "node-nats-streaming";
+import { OrderCreatedEvent, OrderStatus } from "@infiniteideas/common";
+import { OrderCreatedListener } from "../order-created-listener";
+import { natsWrapper } from "../../../nats-wrapper";
+import { Ticket } from "../../../models/ticket";
+
+const setup = async () => {
+  const listener = new OrderCreatedListener(natsWrapper.client);
+
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 99,
+    userId: "asdf",
+  });
+
+  await ticket.save();
+
+  // create fake data object
+  const data: OrderCreatedEvent["data"] = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    version: ticket.version,
+    status: OrderStatus.Created,
+    userId: "alskdfj",
+    expiresAt: "alskdjf",
+    ticket: {
+      id: ticket.id,
+      price: ticket.price,
+    },
+  };
+
+  //@ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, ticket, data, msg };
+};
+
+it("it sets the userId of the ticket", async () => {
+  const { listener, ticket, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  const updatedTicket = await Ticket.findById(ticket.id);
+
+  expect(updatedTicket!.orderId).toEqual(data.id);
+});
+
+it("acks the message", async () => {
+  const { listener, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  expect(msg.ack).toHaveBeenCalled();
+});
---
+++ ticket-app/ticket/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,23 @@
+import { Listener, OrderCreatedEvent, Subjects } from "@infiniteideas/common";
+import { Ticket } from "../../models/ticket";
+import { queueGroupName } from "./queue-group-name";
+import { Message } from "node-nats-streaming";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  subject: Subjects.OrderCreated = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    const ticket = await Ticket.findById(data.ticket.id);
+
+    if (!ticket) {
+      throw new Error("Ticket not found");
+    }
+
+    ticket.set({ orderId: data.id });
+
+    await ticket.save();
+
+    msg.ack();
+  }
+}
