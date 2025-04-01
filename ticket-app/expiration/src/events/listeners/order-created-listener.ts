f59470f7961e6b7b1e28e223525d2b6a336ac09b
certifiedTboy <etosin70@gmail.com>
2025-04-01 02:19:50 +0100

    add expiration service

--- ticket-app/expiration/src/events/listeners/order-created-listener.ts
+++ ticket-app/expiration/src/events/listeners/order-created-listener.ts
@@ -8,10 +8,9 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   queueGroupName = queueGroupName;
 
   async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
-    await expirationQueue.add(
-      { orderId: data.id },
-      { delay: +data.expiresAt - new Date().getTime() }
-    );
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+
+    await expirationQueue.add({ orderId: data.id }, { delay });
 
     msg.ack();
   }
